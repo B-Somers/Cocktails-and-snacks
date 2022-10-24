e0b2fbe75b1e9171800afce69fa900ee9adabb37
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2022-10-24 19:03:51 -0400

    form Elena, fixed typos and stuff written in README

---
+++ assets/scipt.js
@@ -0,0 +1,20 @@
+$.getJSON("www.thecocktaildb.com/api/json/v1/1/search.php?f=a", function(data){
+ console.log(data);
+});
+
+// var fetchBtn = document.getElementById('coctails-btn');
+
+
+// function getApi(){
+// var coctailByName = 'www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+
+//      fetch(coctailByName)
+//      .then(function(response){
+//          return response.json();
+//      }).then(function(data{
+ //   console.log('parsed data!');
+ //   console.log(data);
+//});
+// }
+
+// fetchBtn.addEventListener('click', getApi);
