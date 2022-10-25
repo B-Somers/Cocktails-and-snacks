e7f67ea7cfa439d1da7fe30e614952a310bfb973
Matthew <MatthewGebara@gmail.com>
2022-10-25 02:17:06 -0400

    Button generates random food item

--- assets/scipt.js
+++ assets/scipt.js
@@ -2,6 +2,34 @@ $.getJSON("www.thecocktaildb.com/api/json/v1/1/search.php?f=a", function(data){
  console.log(data);
 });
 
+const mealButton = document.getElementById('mealbtn');
+const mealField = document.getElementById('mealfield');
+
+mealButton.addEventListener('click', () => {
+	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+		.then(res => res.json())
+		.then(res => {
+			createMeal(res.meals[0]);
+		})
+		.catch(e => {
+			console.warn(e);
+		});
+});
+
+const createMeal = meal => {
+
+	const newInnerHTML = `
+		<div class="row">
+			<div class="columns seven">
+				<h4>${meal.strMeal}</h4>
+
+			</div>
+		</div>
+	`;
+
+	mealField.innerHTML = newInnerHTML;
+};
+
 // var fetchBtn = document.getElementById('coctails-btn');
 
 
