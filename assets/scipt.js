5f65d66edde2780cc5345ea30274c0efbdb8a24d
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2022-10-24 20:05:06 -0400

    from Elena

--- assets/scipt.js
+++ assets/scipt.js
@@ -1,20 +1,33 @@
-$.getJSON("www.thecocktaildb.com/api/json/v1/1/search.php?f=a", function(data){
- console.log(data);
-});
+// fetch('www.thecocktaildb.com/api/json/v1/1/search.php')
+//                     .then(response => {
+//                       if (response.ok) return response.json()
+//                       throw new Error('Network response was not ok.')
+//                     })
+//                     .then(data => console.log(data.contents));
+
+
+
+// $.getJSON('https://api.allorigins.win/get?url=' + 
+// encodeURIComponent('www.thecocktaildb.com/api/json/v1/1/search.php?f=a'), function (data) {
+// console.log(data);  });
+
+// $.getJSON("www.thecocktaildb.com/api/json/v1/1/search.php?f=a", function(data){
+//  console.log(data.drinks);
+// });
 
 // var fetchBtn = document.getElementById('coctails-btn');
 
 
-// function getApi(){
-// var coctailByName = 'www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+function getApi(){
+var coctailByName = 'www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
 
-//      fetch(coctailByName)
-//      .then(function(response){
-//          return response.json();
-//      }).then(function(data{
- //   console.log('parsed data!');
- //   console.log(data);
-//});
-// }
-
+     fetch(coctailByName)
+     .then(function(response){
+         return response.json();
+     }).then(function(data){
+   console.log('parsed data!');
+   console.log(data);
+});
+}
+getApi();
 // fetchBtn.addEventListener('click', getApi);
